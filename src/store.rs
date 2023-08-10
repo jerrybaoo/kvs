045124eb1cd1649f0c5b3dfbf31f6da6836a7b50
jerrybaoo <shirui_yj133@163.com>
2023-08-10 18:19:11 +0800

    compress logs

--- src/store.rs
+++ src/store.rs
@@ -1,7 +1,7 @@
 // kv store
 use std::collections::HashMap;
 use std::fs::{self, File};
-use std::io::{BufReader, BufWriter, Read, Seek, SeekFrom::Start, Write};
+use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom::Start, Write};
 use std::path::PathBuf;
 use std::vec;
 
@@ -13,10 +13,12 @@ pub struct KVStore {
 
     readers: HashMap<u32, BufferReader<File>>,
     writer: BufferWriter<File>,
-    last_reader_index: u32,
+    max_reader_id: u32,
 
     index: HashMap<String, TransactionPosition>,
 }
+
+const LOG_MAX_SIZE: u32 = 1000u32;
 
 impl KVStore {
     pub fn new(root_path: &PathBuf) -> Result<Self> {
@@ -25,32 +27,32 @@ impl KVStore {
             fs::create_dir(&path)?;
         }
 
-        let mut storage_files = fs::read_dir(&path)?
+        let mut files = fs::read_dir(&path)?
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, std::io::Error>>()?;
-        storage_files.sort();
+        files.sort();
 
-        if storage_files.is_empty() {
-            storage_files.insert(0, path.join("0.log").to_path_buf());
+        if files.is_empty() {
+            files.insert(0, path.join("0.log").to_path_buf());
         }
 
-        let f = File::options().create(true).read(true).append(true).open(
-            storage_files
-                .last()
-                .ok_or(anyhow!("inner file system error"))?,
-        )?;
+        let active_file = File::options()
+            .create(true)
+            .read(true)
+            .append(true)
+            .open(files.last().ok_or(anyhow!("inner file system error"))?)?;
 
         let writer = BufferWriter::<File> {
-            writer: BufWriter::new(f),
+            writer: BufWriter::new(active_file),
             pos: 0,
         };
 
         let mut readers: HashMap<u32, BufferReader<File>> = HashMap::new();
-        for (i, fp) in storage_files.iter().enumerate() {
+        for (i, fp) in files.iter().enumerate() {
             readers.insert(
                 i as u32,
                 BufferReader::<File> {
-                    reader: BufReader::new(File::open(fp)?),
+                    inner: BufReader::new(File::open(fp)?),
                 },
             );
         }
@@ -60,7 +62,7 @@ impl KVStore {
             readers,
             writer,
             index: HashMap::new(),
-            last_reader_index: (storage_files.len() - 1) as u32,
+            max_reader_id: (files.len() - 1) as u32,
         };
 
         kv_store.load_index()?;
@@ -89,18 +91,18 @@ impl KVStore {
 
     // type(1bit)| timestamp(32bit) | ksz(32bit) | vsz(32bite)| key| value
     fn load_index_from_readers(&mut self) -> Result<()> {
-        for i in 0..=self.last_reader_index {
+        for i in 0..=self.max_reader_id {
             let reader = self.readers.get_mut(&i).ok_or(anyhow!("index error"))?;
             loop {
-                let pos_before = reader.reader.stream_position()? as u32;
+                let pos_before = reader.inner.stream_position()? as u32;
 
                 if let std::result::Result::Ok(t) =
-                    bson::from_reader::<_, Transaction>(&mut reader.reader)
+                    bson::from_reader::<_, Transaction>(&mut reader.inner)
                 {
-                    let pos_after = reader.reader.stream_position()? as u32;
+                    let pos_after = reader.inner.stream_position()? as u32;
 
                     let t_pos = TransactionPosition {
-                        reader_index: i,
+                        log_reader_id: i,
                         offset: pos_before,
                         len: pos_after - pos_before,
                     };
@@ -119,12 +121,21 @@ impl KVStore {
     }
 
     pub fn set(&mut self, key: &str, value: &str) -> Result<()> {
+        self.max_reader_id += 1;
+        if self.writer.pos > LOG_MAX_SIZE {
+            let last_reader = new_log_reader(self.max_reader_id, &self.path)?;
+            let last_writer = new_log_writer(self.max_reader_id, &self.path)?;
+
+            self.readers.insert(self.max_reader_id, last_reader);
+            self.writer = last_writer;
+        }
+
         let transaction: Transaction = Transaction::Set(key.to_string(), value.to_string());
         let bytes = transaction.to_bytes()?;
 
         let size: u32 = self.writer.write(&bytes)?;
         let pos = TransactionPosition {
-            reader_index: self.last_reader_index,
+            log_reader_id: self.max_reader_id,
             offset: self.writer.pos,
             len: size,
         };
@@ -138,7 +149,7 @@ impl KVStore {
         let pos = self.index.get(key).ok_or(anyhow!("key not found"))?;
         let reader = self
             .readers
-            .get_mut(&pos.reader_index)
+            .get_mut(&pos.log_reader_id)
             .ok_or(anyhow!("db maybe breaded"))?;
 
         let mut data = vec![0; pos.len as usize];
@@ -160,6 +171,90 @@ impl KVStore {
 
         Ok(())
     }
+
+    // The main purpose of compression is to remove content that is not pointed to by the index.
+    // The simplest approach is to iterate through the entire index, copy the indexed content
+    // to a newly created compressed file,and update the index to point to the new locations.
+    // Finally, all previous files can be deleted.
+    // If the index is very large, then this will lock the db for a long time.
+    pub fn compress_by_index(&mut self) -> Result<()> {
+        let compress_log_id = self.max_reader_id + 1;
+        let mut compress_log_writer = new_log_writer(compress_log_id, &self.path)?;
+        let mut new_offset = 0;
+
+        for (_, pos) in &mut self.index {
+            if pos.log_reader_id == self.max_reader_id {
+                continue;
+            }
+
+            let reader = self
+                .readers
+                .get_mut(&pos.log_reader_id)
+                .ok_or(anyhow!("index has err"))?;
+            reader.inner.seek(Start(pos.offset as u64))?;
+
+            let mut buf = vec![0; pos.len as usize];
+            reader.inner.read_exact(&mut buf)?;
+
+            let len = io::copy(&mut buf.as_slice(), &mut compress_log_writer.writer)?;
+            pos.log_reader_id = compress_log_id;
+            pos.offset = new_offset;
+
+            new_offset = new_offset + len as u32;
+        }
+
+        compress_log_writer.writer.flush()?;
+
+        let compressed_log_ids: Vec<_> = self
+            .readers
+            .keys()
+            .filter(|&&id| id < self.max_reader_id)
+            .cloned()
+            .collect();
+
+        for id in compressed_log_ids {
+            self.readers.remove(&id);
+            let _ = fs::remove_file(log_path(id, &self.path));
+        }
+
+        self.max_reader_id = compress_log_id;
+        self.readers.insert(
+            compress_log_id,
+            new_log_reader(compress_log_id, &self.path)?,
+        );
+
+        Ok(())
+    }
+
+    // A better way to compress logs may be to read the Transaction from the Reader,
+    // and then query whether the key exists in the index, so that we can split the
+    // compression task into more small tasks.
+    // These small tasks can be designed to be parallelized.
+    pub fn parallel_compress() {}
+}
+
+fn new_log_writer(log_id: u32, path_buf: &PathBuf) -> Result<BufferWriter<File>> {
+    File::options()
+        .read(true)
+        .append(true)
+        .open(log_path(log_id, path_buf))
+        .map(|f| BufferWriter::<File> {
+            writer: BufWriter::new(f),
+            pos: 0,
+        })
+        .map_err(|e| anyhow!(e))
+}
+
+fn new_log_reader(log_id: u32, path_buf: &PathBuf) -> Result<BufferReader<File>> {
+    File::open(log_path(log_id, path_buf))
+        .map(|f| BufferReader::<File> {
+            inner: BufReader::new(f),
+        })
+        .map_err(|e| anyhow!(e))
+}
+
+fn log_path(log_id: u32, path_buf: &PathBuf) -> PathBuf {
+    path_buf.join(format!("{}.log", log_id))
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -180,7 +275,7 @@ impl Transaction {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TransactionPosition {
-    reader_index: u32,
+    log_reader_id: u32,
     offset: u32,
     len: u32,
 }
@@ -192,16 +287,16 @@ struct TransactionIndex {
 }
 
 pub struct BufferReader<T: Read + Seek> {
-    reader: BufReader<T>,
+    inner: BufReader<T>,
 }
 
 impl<T: Read + Seek> BufferReader<T> {
     pub fn read_exact(&mut self, pos: u32, data: &mut [u8]) -> Result<()> {
-        self.reader
+        self.inner
             .seek(Start(pos as u64))
             .map_err(|e| anyhow!(e.to_string()))?;
 
-        self.reader
+        self.inner
             .read_exact(data)
             .map_err(|e| anyhow!(e.to_string()))
     }
